3d440ca4e0620122fa3b3e2b02a07e7841552649
AutoMail Deploy <deploy@automail.local>
2026-05-23 00:29:55 +0700

    fix(build): fix syntax error in account/inboxes.tsx - missing closing brace in map callback

--- artifacts/tempmail/src/pages/account/inboxes.tsx
+++ artifacts/tempmail/src/pages/account/inboxes.tsx
@@ -97,7 +97,8 @@ export default function AccountInboxes() {
                       </Button>
                     </TableCell>
                   </TableRow>
-                ))
+                  );
+                }))
               )}
             </TableBody>
           </Table>
